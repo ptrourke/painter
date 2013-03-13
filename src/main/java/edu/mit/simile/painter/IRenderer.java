8b9abc6b8baf132c24860f5dcd318a12a08f94ce
Ryan Lee <ryanlee@zepheira.com>
2013-03-12 23:04:12 -0700

    reconcile to Google Code repository

--- src/main/java/edu/mit/simile/painter/IRenderer.java
+++
@@ -1,8 +0,0 @@
-package edu.mit.simile.painter;
-
-import java.awt.image.BufferedImage;
-import java.util.Map;
-
-public interface IRenderer {
-	public BufferedImage render(Map<String, String[]> parameters) throws Exception;
-}
